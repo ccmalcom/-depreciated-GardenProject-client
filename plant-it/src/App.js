f9e93fa7f2265804f5d4a04d84bc0b76ef9f8421
avreyes <alyssa.va.reyes@gmail.com>
2021-06-14 15:49:25 -0400

    landing page

--- plant-it/src/App.js
+++ plant-it/src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './App.css';
-import Header from './site/Header';
+//import Header from './site/Header';
 import Sitebar from './site/Sitebar';
 import Login from '../src/components/auth/Login';
 //import Auth from './components/auth/Auth';
 import Footer from './site/Footer';
+import Home from './site/Home';
 
 function App() {
   return (
     <div className="App">
       <Sitebar />
-      <br></br>
-      <Login />
-      <Header />
-      <br></br>
-      <Footer />
+      <Home />
+      {/* <Footer /> */}
+      
+      {/* <Login /> */}
     </div>
   );
 }
