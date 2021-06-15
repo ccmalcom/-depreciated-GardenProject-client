6c4ae10377c20ffbe73bd558996d6ad2e6445b9a
Jason smith <jasonsmith@Jasons-MacBook-Pro.local>
2021-06-15 12:08:59 -0400

    updating files

--- plant-it/src/components/gardens/GardenIndex.js
+++ plant-it/src/components/gardens/GardenIndex.js
@@ -1,76 +1,65 @@
-// import React, { useState, useEffect } from 'react';
-// //import DisplayPlants from './plantTable/PlantTable';
-// //import CreatePlant from './createPlant/CreatePlant';
-// //import PlantView from './PlantView';
-// import { Container, Row } from 'reactstrap';
-// import GardenTable from './GardenTable'
+import React, { useState, useEffect } from 'react';
+//import DisplayPlants from './plantTable/PlantTable';
+//import CreatePlant from './createPlant/CreatePlant';
+//import PlantView from './PlantView';
+import { Container, Row } from 'reactstrap';
+import GardenTable from './GardenTable'
 
-// const GardenIndex = (props) => {
+const GardenIndex = (props) => {
 
-//     const [garden, setGarden] = useState([]);
-//     //const [viewActive, setViewActive] = useState(false);
-//     //const [createGarden, setCreateGarden] = useState(false);
-//     const [gardenToView, setGardenToView] = useState([]);
-//     //const [gardenActive, setGardenActive] = useState(false);
-//     //const [plantToGarden, setPlantToGarden] = useState([]);
+    const [plants, setPlants] = useState([]);
+    const [viewActive, setViewActive] = useState(false);
+    const [plantToView, setPlantToView] = useState([]);
+    const [plantName, setPlantName] = useState(' ')
+    let baseURL = 'http://wd85-plant-it.herokuapp.com/garden/all';
 
-
-//     // const fetchGarden = () => {
-//         fetch('http://wd85-plant-it.herokuapp.com/garden/all', {
-//             method: 'GET',
-//             headers: new Headers({
-//                 'Content-Type': 'application/json'
-//             }),
-//         })
-//             .then(res => res.json())
-//             console.log(res);
-//             .then((Data) => {
-//                 setGarden(Data)
-//                 console.log(data);
-//             })
-//     }
-//     //view functions
-//     const viewGarden = (garden) => {
-//         setGardenToView(garden);
-//         //     console.log(garden);
-//     }
-//     // const viewOn = () => {
-//     //     setViewActive(true)
-//         //}
-//         // const viewOff = () => {
-//         //     setViewActive(false)
-//         }
-
-//         // add to garden functions (ignore this for now)
-//         // const addToGarden = (plant) =>{
-//         //     setPlantToGarden(plant);
-//         // }
-//         // const
-
-//         useEffect(() => {
-//             fetchGarden();
-//             console.log(`garden: ${garden}, viewGarden: ${viewGarden}, viewOn: ${viewOn}, fetchGarden: ${fetchGarden}`);
-//         }, [])
-
-//         return (
-//             <Container>
-//                 <Row>
-//                     <GardenTable garden={garden} viewGarden={viewGarden} gardenToView={gardenToView} viewOn={viewOn} fetchGarden={fetchGarden} token={props.token} />
-
-//                     {viewActive ? <GardenToView GardenToView={gardenToView} viewOff={viewOff} token={props.token} fetchGarden={fetchGarden} /> : <></>}
-//                 </Row>
-//             </Container>
-//         )
-//         //}
-    
-//         // const buttonHandler = () => setCreatePlant(true);
+    const fetchGarden = () => {
+        fetch(baseURL, {
+            method: 'GET',
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': props.token
+            })
+        })
+            .then((res) => res.json())
+            .then((plantData) => {
+                setPlants(plantData)
+                console.log(plants, plantData);
+            })
+    }
 
 
-//         // <>
-//         //     {createPlant ? <CreatePlant setCreatePlant={setCreatePlant} sessionToken={props.sessionToken}/> : null}
-//         //     {!createPlant ? <Button onClick={buttonHandler}>Plant It!</Button> : null}
-//         // </> 
 
-//         //!!! Need to add table above to display plants. Style components or bootstrap table --SC
-        
-//     export default GardenIndex;
+    //view functions
+    const viewPlant = (plant) => {
+        setPlantToView(plant);
+        //     console.log(garden);
+    }
+    const viewOn = () => {
+    setViewActive(true)
+}
+const viewOff = () => {
+    setViewActive(false)
+}
+
+
+
+useEffect(() => {
+    fetchGarden();
+}, [])
+
+return (
+    <Container>
+        <Row>
+            <GardenTable plants={plants} viewPlant={viewPlant} viewOn={viewOn} fetchGarden={fetchGarden} token={props.token} />
+
+            {/* {viewActive ? <GardenView plantToView={plantToView} viewOff={viewOff} token={props.token} fetchGarden={fetchGarden} /> : <></>} */}
+        </Row>
+    </Container>
+)
+}
+
+
+
+
+export default GardenIndex;
